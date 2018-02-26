02c4d2aab87ca84a809115cca56acc447a6f3338
maddog986 <drew@aspinvision.com>
2018-02-26 11:58:43 -0800

    Create rtsp_insta..sh

---
+++ rtsp_insta..sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e #exit if any issues
+
+# Change hostname
+echo "rtsp_viewer" | sudo tee /etc/hostname
+
+# Upgrade the Package Manager Sources
+sudo apt-get -y upgrade
+
+# Update the Package Manger Sources
+sudo apt-get -y update
+
+# Install the Packages
+if [ $(dpkg-query -W -f='${Status}' omxplayer 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get install omxplayer -y
+fi
+
+# Install the Packages
+if [ $(dpkg-query -W -f='${Status}' screen 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get install screen  -y
+fi
+
+# Change directory for script
+cd /home/pi
+
+# Download file using wget
+wget https://github.com/maddog986/raspberry_pi_rtsp/archive/master.zip && unzip master.zip
+
+# Change permissions to enable script
+#chmod +x rtsp_viewer.sh
+sudo chmod 755 /home/pi/rtsp_viewer.sh
+
+# Runs install script to keep script up to date
+(sudo crontab -l; echo "0 5 * * * sudo /home/pi/rtsp_viewer.sh update")| sudo crontab -
+
+# Reboots system at 6am everyday
+(sudo crontab -l ; echo "0 6 * * * sudo reboot")| sudo crontab -
+
+# Runs script every minute between 6am and 11pm to make sure feeds are running
+(sudo crontab -l ; echo "* 6-23 * * * sudo /home/pi/rtsp_viewer.sh repair")| sudo crontab -
+
+# Makes sure script gets started upon boot
+(sudo crontab -l ; echo "@reboot sudo /home/pi/rtsp_viewer.sh start")| sudo crontab -
