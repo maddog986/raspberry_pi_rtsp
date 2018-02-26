529c68080a1bab5535e86e0d633e62c863d1c142
maddog986 <drew@aspinvision.com>
2018-02-26 12:33:38 -0800

    Update rtsp_install.sh

--- rtsp_install.sh
+++ rtsp_install.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
+#install command:
+#mkdir /home/pi/rtsp_vewier && cd /home/pi/rtsp_vewier && wget https://github.com/maddog986/raspberry_pi_rtsp/archive/master.zip && unzip master.zip && mv raspberry_pi_rtsp-master/* . && rm -R raspberry_pi_rtsp-master && rm master.zip && chmod -R 755 /home/pi/rtsp_viewer && sudo ./rtsp_install.sh
+
 set -e #exit if any issues
 
 # Change hostname
-echo "rtsp_viewer" | sudo tee /etc/hostname
+echo "camera_viewer" | tee /etc/hostname
 
 # Upgrade the Package Manager Sources
-sudo apt-get -y upgrade
+apt-get -y upgrade
 
 # Update the Package Manger Sources
-sudo apt-get -y update
+apt-get -y update
 
 # Install the Packages
 if [ $(dpkg-query -W -f='${Status}' omxplayer 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  sudo apt-get install omxplayer -y
+  apt-get install omxplayer -y
 fi
 
 # Install the Packages
 if [ $(dpkg-query -W -f='${Status}' screen 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  sudo apt-get install screen  -y
+  apt-get install screen  -y
 fi
 
 # Runs install script to keep script up to date
-(sudo crontab -l; echo "0 5 * * * sudo /home/pi/rtsp_viewer/rtsp_viewer.sh update")| sudo crontab -
+(crontab -l; echo "0 5 * * * sudo /home/pi/rtsp_viewer/rtsp_viewer.sh update")| crontab -
 
 # Reboots system at 6am everyday
-(sudo crontab -l ; echo "0 6 * * * sudo reboot")| sudo crontab -
+(crontab -l ; echo "0 6 * * * sudo reboot")| crontab -
 
 # Runs script every minute between 6am and 11pm to make sure feeds are running
-(sudo crontab -l ; echo "* 6-23 * * * sudo /home/pi/rtsp_viewer/rtsp_viewer.sh repair")| sudo crontab -
+(crontab -l ; echo "* 6-23 * * * sudo /home/pi/rtsp_viewer/rtsp_viewer.sh repair")| crontab -
 
 # Makes sure script gets started upon boot
-(sudo crontab -l ; echo "@reboot sudo /home/pi/rtsp_viewer/rtsp_viewer.sh start")| sudo crontab -
+(crontab -l ; echo "@reboot sudo /home/pi/rtsp_viewer/rtsp_viewer.sh start")| crontab -
+
+# Reboot so changes take effect
+reboot
